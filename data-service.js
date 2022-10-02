d0970b69b6dfdc2aaabd3602cf7b09fd37dbec24
Gazal100 <gazalgarg8065@gmail.com>
2022-10-01 23:27:57 -0400

    app changes

---
+++ data-service.js
@@ -0,0 +1,48 @@
+let fs = require('fs');
+let employees = [];
+let departments = [];
+
+module.exports.initialize = function(){
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/employees.json',(err,data)=>{
+            if (err) reject("Failure to read file employees.json!");
+            else {
+                employees = JSON.parse(data)
+                fs.readFile('./data/employees.json',(err,data)=>{
+                    if (err) reject("Failure to read file employees.json!");
+                    else departments = JSON.parse(data);
+                        resolve("Successful")});
+                }
+        })
+    })
+}
+
+module.exports.getAllEmployees = function(){
+    return new Promise(function(reject, resolve){
+        if(employees.length == 0) 
+            reject("no results found");
+        else
+            resolve(employees);
+    })
+}
+
+module.exports.getManagers = function(){
+    return new Promise(function(reject, resolve){
+        let managers = employees.filter(function(employee){
+            return employee.isManager;
+        });
+        if(managers.length == 0)
+            reject("no results found");
+        else
+            resolve(managers);
+    })
+}
+
+module.exports.getDepartments = function(){
+    return new Promise(function(reject, resolve){
+        if(departments.length == 0)
+            reject("no results found");
+        else
+            resolve(departments);
+    })
+}
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Gazal Garg     Student ID: 107140212      Date: October01, 2022
+*
+* Your app’s URL (from Cyclic) : https://teal-clever-walrus.cyclic.app/
+*
+*************************************************************************/
+
 //using express module
 var express = require("express");
+var data = require("./data-service.js");
 var app = express();
-
-var path = require("path")
+var path = require("path");
 
 //listen on process.env.PORT || 8080
 var HTTP_PORT = process.env.PORT || 8080;
@@ -15,12 +28,43 @@ app.use(express.static('public')); //to get accurate css file
 
 //Setting route for the home page
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 //Setting route for the about page
 app.get("/about", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+//Setting up route to get all employees
+app.get("/employees", function(req, res){
+    data.getAllEmployees()
+    .then((data) => {res.json(data)})
+    .catch((err) => {res.json({message: err})})
 })
 
-app.listen(HTTP_PORT, onHttpStart);
+//Setting up route to get only the managers
+app.get("/managers", function(req, res){
+    data.getManagers()
+    .then((data) => {res.json(data)})
+    .catch((err) => {res.json({message: err})})
+});
+
+//Setting up route to get the departments
+app.get("/departments", function(req, res){
+    data.getDepartments()
+    .then((data) => {res.json(data)})
+    .catch((err) => {res.json({message: err})})
+});
+
+//Set up a route if no route is matched
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname,"/views/404.html"));
+});
+
+//creating local server
+data.initialize().then(()=>{
+    app.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => {
+    console.log(err);
+})
